f5d603a4697dcaabb81f72efa602444d20da169d
kerrodia <5658@holbertonstudents.com>
2022-11-09 03:46:00 -0800

    edited main.h

--- main.h
+++ main.h
@@ -1,25 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
-
-int _printf(const char *format, ...);
-
+/**
+  * struct print - structure for printing
+  * @type: type of what will be printed
+  * @f: function to perform
+  *
+  */
 typedef struct print
 {
-        char *type;
-        void (*f)(va_list list);
+char *type;
+int (*f)(va_list list);
 } print;
 
-int spring_print(va_list list);
-int char_print(va_list list);
-int percent_print(va_list list);
 int _putchar(char c);
+int percent_print(va_list list);
+int char_print(va_list list);
+int string_print(va_list list);
 int _printf(const char *format, ...);
-int get_print(const char* format, print prints[], va_list list);
+int get_print(const char *format, print prints[], va_list list);
 
 #endif
