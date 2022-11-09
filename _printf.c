5733eb48190ef3094109328cda30f878193a39d7
soOwasTaken <444deph12@gmail.com>
2022-11-09 13:29:37 +0000

    fixing merge

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
 
 /**
  * get_print - use fonctions to print using array
@@ -30,10 +29,6 @@ int get_print(char getChar, va_list list)
 
 /**
  * _printf - print by format, and arguments.
-=======
-/**
- * _printf - prints an entire string
->>>>>>> cd3094400026798484ed4552ea6286b345520166
  * @format: arguments in our printf function
  *
  * Return: size of what will be printed
@@ -41,7 +36,6 @@ int get_print(char getChar, va_list list)
 
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
 	unsigned int i;
 	int nFonctionCalled = 0, charPrinted = 0;
 	va_list list;
@@ -77,22 +71,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(list);
 	return (charPrinted); /* return lenght of what will be printed */
-=======
-va_list list;
-int count = -1;
-
-print prints[] = {
-{"c", char_print},
-{"s", string_print},
-{"%", percent_print},
-{NULL, NULL}
-};
-if (format != NULL)
-{
-va_start(list, format);
-count = get_print(format, prints, list);
-va_end(list);
-}
-return (count);
->>>>>>> cd3094400026798484ed4552ea6286b345520166
 }
